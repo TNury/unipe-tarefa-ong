5cec73e68f01c57a30db835ab3195e569c848a1c
TNury <yurdesou@gmail.com>
2025-10-26 15:37:10 -0300

    feat: improve header

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > 100) {
-      header.style.backgroundColor = 'rgba(34, 38, 47, 0.95)';
+      header.style.backgroundColor = '#252A34B2';
       header.style.backdropFilter = 'blur(10px)';
     } else {
       header.style.backgroundColor = 'transparent';
